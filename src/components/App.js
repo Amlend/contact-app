ccc1d7c9daf7e3a0734be462f11adb5cd8184fc2
Amlend <amlendsingh@gmail.com>
2024-05-03 02:50:01 +0530

    added-contact-card

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,23 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Amlend",
+      email: "amlendsingh@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Ram",
+      email: "ram@gmail.com",
+    },
+  ];
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
