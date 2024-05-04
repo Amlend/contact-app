21d2786b1539c4b1f50de0ee0f87be8daf5ec6e6
Amlend <amlendsingh@gmail.com>
2024-05-04 22:46:36 +0530

    added-uuid-and-delete-contact

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -16,14 +17,21 @@ function App() {
 
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+    setContacts(newContactList);
   };
 
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import user from "../images/profile.png";
 
 const CardContact = (props) => {
-  const { name, email } = props.contact;
+  const { id, name, email } = props.contact;
   return (
     <div className="item">
       <img className="ui avatar image" src={user} alt="user" />
@@ -13,6 +13,7 @@ const CardContact = (props) => {
       <i
         className="trash alternate outline icon"
         style={{ color: "red", marginTop: "7px", float: "right" }}
+        onClick={() => props.clickHandler(id)}
       ></i>
     </div>
   );
